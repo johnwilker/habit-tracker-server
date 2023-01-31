ee929a234e4d67d91cf6684e4796a8fbc651cad1
John Wilker <johnwilker2@gmail.com>
2023-01-30 21:30:47 -0300

    back end missing debug

---
+++ src/notifications-routes.ts
@@ -0,0 +1,8 @@
+import WebPush from 'web-push';
+import { FastifyInstance } from 'fastify';
+
+WebPush.generateVAPIDKeys
+
+export async function notificationRoutes(app: FastifyInstance) {
+  
+}
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import cors from '@fastify/cors';
+import { appRoutes } from "./routes";
+import { notificationRoutes } from "./notifications-routes";
+
+const app = fastify();
+
+app.register(cors);
+app.register(appRoutes);
+app.register(notificationRoutes);
+
+app.listen({
+  port: 3333, host: '0.0.0.0'
+}).then(() => {
+  console.log('HTTP Server running!')
+})
